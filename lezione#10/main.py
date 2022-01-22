20e869454f357e73cb47db00ad2ac1576c95cddd
Serena Sensini <serena.sensini@gmail.com>
2022-01-22 19:48:16 +0100

    Update main.py

--- lezione#10/main.py
+++ lezione#10/main.py
@@ -145,3 +145,162 @@ print(classification_report(y_test, lr_pred))
 y = ["It was a great experience"]
 y = cv.transform(y)
 print(lr.predict(y))
+
+
+# Database
+## Postgres with Psycopg2
+
+import psycopg2
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(host="localhost",
+             database="suppliers",
+             user="postgres",
+             password="Abcd1234")
+		
+        # create a cursor
+        cur = conn.cursor()
+        
+	# execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+       
+	# close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+         
+def get_users():
+    """ query data from the users table """
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(host="localhost",
+             database="suppliers",
+             user="postgres",
+             password="Abcd1234")
+		
+        # create a cursor
+        cur = conn.cursor()
+        cur.execute("SELECT user_id, user_name FROM user ORDER BY user_name")
+        print("The number of users: ", cur.rowcount)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+                  
+
+def insert_user(user_name):
+    """ insert a new user into the users table """
+    sql = """INSERT INTO users(user_name)
+             VALUES(%s) RETURNING user_id;"""
+    conn = None
+    user_id = None
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(host="localhost",
+             database="suppliers",
+             user="postgres",
+             password="Abcd1234")
+		
+        # create a cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql, (user_name,))
+        # get the generated id back
+        user_id = cur.fetchone()[0]
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return user_id
+
+def delete_user(part_id):
+    """ delete user by user_id """
+    conn = None
+    rows_deleted = 0
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(host="localhost",
+             database="suppliers",
+             user="postgres",
+             password="Abcd1234")
+		
+        # create a cursor
+        cur = conn.cursor()
+        # execute the DELETE statement
+        cur.execute("DELETE FROM users WHERE user_id = %s", (part_id,))
+        # get the number of updated rows
+        rows_deleted = cur.rowcount
+        # Commit the changes to the database
+        conn.commit()
+        # Close communication with the PostgreSQL database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return rows_deleted
+
+
+if __name__ == '__main__':
+    connect()
+    get_users()
+    insert_user()
+    delete_user()
+
+
+# MySQL con SQLAlchemy
+
+import sqlalchemy as db
+
+engine = db.create_engine("mysql+pymysql://user:pwd@localhost/test", echo=False)
+metadata = db.MetaData()
+
+# USO SELECT
+persone = db.Table('persone', metadata, autoload=True, autoload_with=engine)
+#Equivalent to 'SELECT * FROM PERSONE'
+query = db.select([persone])
+connection = engine.connect()
+result = connection.execute(query)
+for row in result:
+    print(row)
+
+# QUERY
+t = db.text("SELECT * FROM persone")
+result2 = connection.execute(t)
+for row in result2:
+    print(row)
